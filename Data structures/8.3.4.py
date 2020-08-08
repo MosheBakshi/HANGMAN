e875e6daee816c31f093730c8f6f24b1bc157748
MosheBakshi <moshetrojan@gmail.com>
2020-08-08 17:16:56 +0300

    "done 8.3.4 - Creating an inverse dictionary: every value becomes a key and holds list of key that had that value."

---
+++ Data structures/8.3.4.py
@@ -0,0 +1,23 @@
+def inverse_dict(my_dict):
+    """
+    :param my_dict:
+    :return: Inverse dictionary - every value becomes a key and holds list of key that had that value.
+    """
+    inverse_dictionary = {}
+    for item in my_dict.items():
+        if item[1] in inverse_dictionary:
+            inverse_dictionary[item[1]].append(item[0])
+        else:
+            inverse_dictionary[item[1]] = []
+            inverse_dictionary[item[1]].append(item[0])
+    return inverse_dictionary
+
+
+def main():
+    print(inverse_dict.__doc__)
+    course_dict = {'I': 3, 'love': 3, 'self.py!': 2}
+    print(inverse_dict(course_dict))
+
+
+if __name__ == '__main__':
+    main()
